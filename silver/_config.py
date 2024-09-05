5c328235ff2400bfd86407daa0ef8c9205581ba3
janusmiracle <janusmiracle@outlook.com>
2024-09-05 12:40:15 -0400

    setup default config

--- silver/_config.py
+++ silver/_config.py
@@ -4,24 +4,44 @@ import platform
 
 from datetime import datetime
 
-__application__ = "silver"
-__version__ = "0.1.0"
-__author__ = "janusmiracle"
+from _version import __version__
 
-CONFIG = {
+__application__ = "silver"
+
+DEFAULT_CONFIG = {
     "run_date": datetime.now().isoformat(),
-    "application_name": __application__,
+    "application": __application__,
     "version": __version__,
-    "full_application_name": f"{__application__} {__version__}",
+    "full_application": f"{__application__} {__version__}",
     "system": {
         "os": platform.system(),
         "os_version": platform.version(),
         "architecture": platform.architecture()[0],
     },
     "config": {
-        "input_source": None,
-        "operation_mode": None,
-        "output_format": "JSON",
+        "input_source": 0,
+        "operation_mode": 2,  # Set to 2 during testing.
+        "output_format": 0,
     },
     "execution_time": None,
 }
+
+CONFIG_OPTIONS = """
+Input Source:
+    0: file – Local file path.
+    1: url – Remote URL.
+    2: stream – Opened file stream object.
+    3: bytes – Raw bytes input.
+
+Operation Mode:
+    0: minimal – Extracts only key information.
+    1: default – Extracts all information.
+    2: absolute – Extracts all available information along with config settings and debugging prints.
+
+
+Output Format:
+    0: JSON 
+    1: YAML
+    2: XML 
+    3: TOML
+    """
