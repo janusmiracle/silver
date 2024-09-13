17ddb330ddee2bb85c4ec6c9a978085b5833f776
janusmiracle <janusmiracle@outlook.com>
2024-09-13 02:49:32 -0400

    fix: add sanity checks for `strc` chunk

--- silver/audio/wave/ck_strc.py
+++ silver/audio/wave/ck_strc.py
@@ -8,11 +8,15 @@ import struct
 from dataclasses import dataclass
 from typing import List
 
+from .errors import PerverseError
+
 from silver.utils import get_sign
 
 # The implementation might not be accurate due to lack of documentation.
 # Any insights or corrections are welcome.
 # No testing will be created for this chunk.
+
+STRC_CHUNK_LOCATION = "['strc']"
 
 
 @dataclass
@@ -22,7 +26,6 @@ class SliceBlock:
     sample_position: int
     sample_position2: int
     data3: int
-
     data4: int
 
 
@@ -30,6 +33,8 @@ class SliceBlock:
 class WaveStrcChunk:
     identifier: str
     size: int
+    sanity: List[PerverseError]
+
     unknown1: int
     slice_count: int
     unknown2: int
@@ -56,12 +61,16 @@ class WaveStrc:
         # -- Strc chunk info fields
         self.strc = self.get_strc()
 
+        # -- Sanity
+        self.sanity = None
+
     def get_strc(self) -> WaveStrcChunk:
         """
         Decodes the provided ['strc' / STRC] chunk data.
         """
         sign = get_sign(self.byteorder)
         header_pattern = f"{sign}IIIIIII"
+        self.sanity = []
         (
             unknown1,
             slice_count,
@@ -79,10 +88,11 @@ class WaveStrc:
 
         for i in range(slice_count):
             if offset + 32 > total_data_size:
-                # TODO: make this a sanity check
-                print(
-                    f"Warning: Not enough data left to unpack slice {i}. Missing or padded slice."
+                location = f"{STRC_CHUNK_LOCATION} -- SLICE {i}"
+                error_message = (
+                    "NOT ENOUGH DATA TO UNPACK SLICE -- MISSING OR PADDED SLICE."
                 )
+                self.sanity.append(PerverseError(location, error_message))
                 break
 
             try:
@@ -90,7 +100,9 @@ class WaveStrc:
                     struct.unpack(slice_pattern, self.data[offset : offset + 32])
                 )
             except struct.error as e:
-                print(f"Error unpacking slice {i}: {e}")
+                location = f"{STRC_CHUNK_LOCATION} -- SLICE {i}"
+                error_message = "SLICE {i} COULD NOT BE UNPACKED: {e}"
+                self.sanity.append(PerverseError(location, error_message))
                 break
 
             slice_block = SliceBlock(
@@ -105,13 +117,14 @@ class WaveStrc:
             offset += 32
 
         if len(slice_blocks) != slice_count:
-            # TODO: make this to a sanity check
-            print(
-                f"Warning: Expected {slice_count} slices, but only {len(slice_blocks)} were unpacked."
-            )
+            location = f"{STRC_CHUNK_LOCATION} -- SLICE BLOCKS"
+            error_message = f"EXPECTED {slice_count} SLICES -- GOT {len(slice_blocks)}."
+            self.sanity.append(PerverseError(location, error_message))
+
         return WaveStrcChunk(
             identifier=self.identifier,
             size=self.size,
+            sanity=self.sanity,
             unknown1=unknown1,
             slice_count=slice_count,
             unknown2=unknown2,
